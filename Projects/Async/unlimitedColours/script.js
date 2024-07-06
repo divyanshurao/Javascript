cb8bf01b0998fe5785693150e29521e177b20ae9
divyanshurao <divyanshu.co19@nsut.ac.in>
2024-07-06 23:59:31 +0530

    UnlimitedColours Project

---
+++ Projects/Async/unlimitedColours/script.js
@@ -0,0 +1,19 @@
+function randomColour(){
+    let color = '#';
+    const hex = '0123456789ABCDEF'
+    for(i=0; i<6; i++){
+        color += hex[(Math.floor(Math.random()*16))];
+    }
+    return color;
+}
+let setBackgroundEvent;
+
+document.getElementById('start').addEventListener('click',()=>{
+       setBackgroundEvent = setInterval(()=>{
+       document.body.style.backgroundColor = randomColour();
+    }, 1000);
+})
+
+document.getElementById('stop').addEventListener('click', ()=>{
+    clearInterval(setBackgroundEvent);
+})
