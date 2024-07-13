10125d5d811e8cda7e36bd5bc979c7c8ad4dd5e6
divyanshurao <divyanshu.co19@nsut.ac.in>
2024-07-14 00:46:16 +0530

    apiRequest

--- Projects/KeyboardEvent/script.js
+++ Projects/KeyboardEvent/script.js
@@ -1,6 +1,7 @@
 const insert = document.getElementById('insert');
 
 window.addEventListener('keydown',(e)=>{
+    console.log(e);
     insert.innerHTML = `<div class="color">
     <table>
     <tr>
